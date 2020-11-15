3dde61e55f0a46aeeb1f849c29cd1c85f734e0a9
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-15 07:02:23 +0000

    [Add] 投稿削除機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e849d0cb711d6a4cb225b7a5e5beb03530b32d626df23c3dc2feb5e97b434c5fe6a68412ac62ef753ae8595980d7acf375281ff12041b659c8eac04c1faf1ee6'
